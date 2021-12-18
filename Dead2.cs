80e235b80648f6df109360ec484017235780132a
BadgerFat <94040711+BadgerFat@users.noreply.github.com>
2021-12-18 11:46:47 +0700

    Create Dead2.cs

---
+++ Dead2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dead2 : MonoBehaviour {
+    public int Health = 10;
+    public Text tex;
+
+    void OnCollisionEnter2D(Collision2D theCollision)
+    {
+        if (thecollision.gameObject.name.Constains('кулак1'))
+            health -= 1;
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+            tex.text"You Won";
+        }
+    }
+}
