227e9bb3239292f06779635da08117759ce73b2c
BadgerFat <94040711+BadgerFat@users.noreply.github.com>
2021-12-18 11:46:55 +0700

    Create kylak1.cs

---
+++ kylak1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kylak1 : MonoBehaviour {
+    public float lifetime = 0.18f;
+    public float speed = 5f;
+    public int damage = 1;
+    private Transform player;
+    // Start is called before the first frame update
+    void Start() {
+        Destroy (gameObject, lifetime);
+        player = GameObject.Find ("Эльшан").transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 delta = player.position - transform.position;
+        delta.Normalize();
+        float move = speed * Time.deltaTime;
+        transform.position = transform.position + (delta * move);
+
+    }
+}
